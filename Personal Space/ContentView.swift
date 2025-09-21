de48bc3114b04d0be9131ba34935133cae02fc18
Penn <450711123@qq.com>
2025-09-21 20:11:12 +0800

    Initial Commit

---
+++ Personal Space/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Personal Space
+//
+//  Created by Penn on 2025/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Personal Space/Personal_SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_SpaceApp.swift
+//  Personal Space
+//
+//  Created by Penn on 2025/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Personal_SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
